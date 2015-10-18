8411fd5b843947dc0bb6b4bae3189892c35d99a9
Stalf <kssgel@yandex.ru>
2015-10-18 16:21:18 +0500

    Переключил уровень логирования по умолчанию

--- core/src/com/droidwars/game/DroidWarsGame.java
+++ core/src/com/droidwars/game/DroidWarsGame.java
@@ -1,6 +1,8 @@
 package com.droidwars.game;
 
+import com.badlogic.gdx.Application;
 import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
 import com.droidwars.game.screens.GameScreen;
 import com.droidwars.game.screens.MainMenuScreen;
 
@@ -8,6 +10,9 @@ public class DroidWarsGame extends Game {
 
     @Override
     public void create() {
+
+        Gdx.app.setLogLevel(Application.LOG_INFO);
+
 //        setScreen(new MainMenuScreen(this));
         setScreen(new GameScreen(this));
     }
